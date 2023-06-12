04bf079a5fd25ac30ce3e506e15165a1192eb730
AsimGi <115151895+AsimGi@users.noreply.github.com>
2023-06-12 16:38:00 +0530

    Update streamlit_app.py
    Added Special

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,10 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.text("Idly")
-streamlit.text("Dosa")
-streamlit.text("Vada")
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toaast')
 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
