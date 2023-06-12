cf803c2eb71094210087e076dd537e16c53f3a9a
AsimGi <115151895+AsimGi@users.noreply.github.com>
2023-06-12 16:32:14 +0530

    Update streamlit_app.py
    Added Bf Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit
+
+streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+
+streamlit.Text("Idly")
+streamlit.Text("Dosa")
+streamlit.Text("Vada")
 
