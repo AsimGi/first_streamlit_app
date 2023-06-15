77754152d53f1ebf0d491e33c168b88654ce046e
AsimGi <115151895+AsimGi@users.noreply.github.com>
2023-06-15 14:27:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,8 @@ my_cur = my_cnx.cursor()
 fruit_add = streamlit.text_input('What fruit would you like to  add?')
 if(fruit_add.strip() != ""):
   my_cur.execute("Insert into fruit_load_list values('" +fruit_add +"')" )
-  my_cur.execute("SELECT * from fruit_load_list")
-  my_data_rows = my_cur.fetchall()
-  my_cur.execute("SELECT * from fruit_load_list")
+  
+my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
